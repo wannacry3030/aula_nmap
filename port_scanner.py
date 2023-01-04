ee130b829f9f3014f4064d6b011f4d147daa9f12
wannacry3030 <wannacry.bat@proton.me>
2023-01-04 17:22:26 -0300

    modified:   port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,2 +1,9 @@
 import socket
 import sys
+
+host = "google.com"
+port = 80
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+code = client.connect_ex((host, port))
+if code == 0:
+    print("[+] {} open".format(port))

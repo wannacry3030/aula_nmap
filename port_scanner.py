222bfa7bfe009f84200c311300403a833d063f70
wannacry3030 <wannacry.bat@proton.me>
2023-01-04 17:46:11 -0300

    modified:   port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -2,11 +2,11 @@ import socket
 import sys
 
 
-def scan(host, ports, timeout=0.5):
+def scan(host, ports):
     try:
         for port in ports:
           client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-          client.settimeout(timeout)
+          client.settimeout(0.5)
           code = client.connect_ex((host, int(port)))
           if code == 0:
               print("[+] {} open".format(port))
@@ -18,8 +18,7 @@ if __name__ == "__main__":
         host = sys.argv[1]
         if len(sys.argv) >= 3:
             ports = sys.argv[2].split(",")
-            if len(sys.argv) >= 4:
-                sys.argv[4]
+                
         else:
             ports = [21, 22, 23, 25, 80, 443, 445, 8080, 8443, 3306, 139, 135]
         scan(host, ports)

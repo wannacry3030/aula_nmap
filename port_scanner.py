7e62904fc1e1537f02673f4b51c4fd2f37c52d06
wannacry3030 <wannacry.bat@proton.me>
2023-01-04 17:37:14 -0300

    modified:   port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,12 +1,24 @@
 import socket
 import sys
 
-host = "google.com"
-ports = [21, 22, 23, 25, 80, 443, 445, 8080, 8443, 3306, 139, 135]
 
-for port in ports:
-  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  client.settimeout(0.05)
-  code = client.connect_ex((host, port))
-  if code == 0:
-      print("[+] {} open".format(port))
+def scan(host, ports, timeout=0.5):
+    for port in ports:
+      client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      client.settimeout(timeout)
+      code = client.connect_ex((host, int(port)))
+      if code == 0:
+          print("[+] {} open".format(port))
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        host = sys.argv[1]
+        if len(sys.argv) >= 3:
+            ports = sys.argv[2].split(",")
+            if len(sys.argv) >= 4:
+                sys.argv[4]
+        else:
+            ports = [21, 22, 23, 25, 80, 443, 445, 8080, 8443, 3306, 139, 135]
+        scan(host, ports)
+    else:
+        print("Usage: python port_scanner.py google.com 22,23,80,443")
